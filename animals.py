0655226998f768e3246aa389010b7cd3d7888865
Hugo <h.ilarraz@groupecerise.fr>
2023-11-17 16:31:39 +0100

    refacto(animals): Fix PEP8 conventions

--- animals.py
+++ animals.py
@@ -6,20 +6,18 @@ class Rabbit:
         self.last_mate = None  # Track the last mate to avoid immediate re-mating
 
     def age_one_week(self):
-        self.age += 1/52  # Age in years
+        self.age += 1 / 52  # Age in years
 
     def eat(self, garden):
-        if garden.harvest_carrot():
-            self.has_eaten = True
-        else:
-            self.has_eaten = False
+        self.has_eaten = garden.harvest_carrot()
 
     def can_reproduce(self, current_date):
         return self.age >= 1 and self.gender == 'female' and current_date.month in [4, 7]
 
     def is_dead(self):
         max_age = 4 if not self.has_eaten else 6
-        return self.age > max_age or (not self.has_eaten and self.age > 2/52)
+        return self.age > max_age or (not self.has_eaten and self.age > 2 / 52)
+
 
 class Carrot:
     def __init__(self):
