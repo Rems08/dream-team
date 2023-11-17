f625075e26f94964dd51a8271e34c347a395edbb
Rémy MASSIET <remy.massiet@savoye.com>
2023-11-17 16:30:41 +0100

    Add plotlib interface

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+import matplotlib.pyplot as plt
 from garden import Garden
 
-# Simulate over 6 years
+def plot_garden_evolution(garden):
+    weeks = list(range(len(garden.rabbit_count)))
+    plt.figure(figsize=(10, 6))
+
+    # Plotting rabbits and carrots over time
+    plt.plot(weeks, garden.rabbit_count, label='Rabbits')
+    plt.plot(weeks, garden.carrot_count, label='Carrots')
+    
+    # Optionally, plot reproduction events
+    plt.plot(weeks, garden.reproduction_count, label='Reproduction Events')
+
+    plt.xlabel('Weeks')
+    plt.ylabel('Count')
+    plt.title('Garden Evolution Over Time')
+    plt.legend()
+    plt.show()
+
+# Run the simulation
 garden = Garden()
-for week in range(6 * 52):  # 6 years in weeks
+for week in range(6 * 52):
     garden.weekly_update()
 
-print(garden)  # Final state of the garden after 6 years
+# Plot the results
+plot_garden_evolution(garden)
